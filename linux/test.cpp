9965feca14bb7ed6bb798c21792c081137642d27
Ts-sound <1564264931@qq.com>
2024-05-21 16:02:22 +0800

    fix

--- linux/test.cpp
+++ linux/test.cpp
@@ -14,14 +14,14 @@ int main() {
               << std::endl;
     ret = enumerate_relay_boards("", 1, 1);
     std::cout << "ret = " << ret << std::endl;
-    std::cout << "---------------------------------" << ret << std::endl;
+    std::cout << "---------------------------------"  << std::endl;
   }
 
   {
     std::cout << "get_relay_board_count(void); " << std::endl;
     int cnt = get_relay_board_count();
-    std::cout << "ret = " << ret << std::endl;
-    std::cout << "---------------------------------" << ret << std::endl;
+    std::cout << "cnt = " << cnt << std::endl;
+    std::cout << "---------------------------------" << std::endl;
 
     auto boards = get_relay_boards();
     for (size_t i = 0; i < cnt; i++) {
@@ -31,7 +31,7 @@ int main() {
                 << "\t serial : " << boards[i].serial
                 << "\t state : " << boards[i].state << std::endl;
     }
-    std::cout << "---------------------------------" << ret << std::endl;
+    std::cout << "---------------------------------"  << std::endl;
 
     std::cout << "operate_relay(const char *path,unsigned char relay, unsigned "
                  "char state,int debug);  "
